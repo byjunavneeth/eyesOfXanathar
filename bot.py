76292248f8c126564e2bb63c84d068fa552bcc5c
byjunavneeth <byjunavneeth@gmail.com>
2026-03-01 12:24:58 +0530

    flask removed

--- bot.py
+++ bot.py
@@ -3,19 +3,7 @@ import random
 import discord
 from discord import app_commands
 import threading
-from flask import Flask
 import os
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    return "Bot is alive"
-
-def run_web():
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
-
-threading.Thread(target=run_web).start()
 
 
 TOKEN = os.getenv("TOKEN")
